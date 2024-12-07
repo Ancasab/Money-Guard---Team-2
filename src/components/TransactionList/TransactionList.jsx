9ac647d19b4387eb4b41dc2cfb9aaaab03239253
CatalinaCap <c.cata1706@gmail.com>
2024-12-07 21:08:51 +0000

    pas1

--- src/components/TransactionList/TransactionList.jsx
+++ src/components/TransactionList/TransactionList.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import styles from './TransactionList.module.css';
 
-const TransactionList = () => {
+const TransactionsList = ({ transactions = [], onDelete, onEdit }) => {
   return (
-    <div>TransactionList</div>
-  )
-}
+    <div className={styles.tableContainer}>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Type</th>
+            <th>Category</th>
+            <th>Comment</th>
+            <th>Sum</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map(transaction => (
+            <tr key={transaction.id}>
+              <td>{transaction.date}</td>
+              <td>{transaction.type}</td>
+              <td>{transaction.category}</td>
+              <td>{transaction.comment}</td>
+              <td>{transaction.sum}</td>
+              <td>
+                <button onClick={() => onEdit(transaction.id)}>Edit</button>
+                <button onClick={() => onDelete(transaction.id)}>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-export default TransactionList
+export default TransactionsList;
