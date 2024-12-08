9c209f50451aa20f1a8876e10adaff5a04d0f854
Calin Daria Andreea <calindariaandreea@gmail.com>
2024-12-08 15:34:05 +0200

    .

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getTransactions } from '../../redux/Transactions/operations';
+import { getTransactionsCategories } from '../../redux/Statistics/operations';
+import TransactionList from '../TransactionList/TransactionList';
+import AddButton from '../AddButton/AddButton';
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+function Home() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getTransactions());
+        dispatch(getTransactionsCategories());
+    }, [dispatch]);
+    return (
+        <>
+            <TransactionList />
+            <AddButton></AddButton>
+        </>
+    );
 }
 
-export default Home
+export default Home;
