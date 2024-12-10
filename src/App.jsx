95e3fd8f552c5e036a2dbde6dce3b84fb70cd527
gabrielaraducan1606 <165211880+gabrielaraducan1606@users.noreply.github.com>
2024-12-10 19:37:14 +0200

    commit5

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import React, { useState } from "react";
-import AddTransactionModal from "./components/ModalAddTransaction/ModalAddTransaction";
-import EditTransactionModal from "./components/ModalEditTransaction/ModalEditTransaction";
+//import React, { useState } from "react";
+//import AddTransactionModal from "./components/ModalAddTransaction/ModalAddTransaction";
+//import EditTransactionModal from "./components/ModalEditTransaction/ModalEditTransaction";
 
-const App = () => {
-  const [isAddModalOpen, setAddModalOpen] = useState(false);
-  const [isEditModalOpen, setEditModalOpen] = useState(false);
+//const App = () => {
+  //const [isAddModalOpen, setAddModalOpen] = useState(false);
+  //const [isEditModalOpen, setEditModalOpen] = useState(false);
 
-  return (
-    <div>
-      <button onClick={() => setAddModalOpen(true)}>Add Transaction</button>
-      <button onClick={() => setEditModalOpen(true)}>Edit Transaction</button>
+ // return (
+    //<div>
+      //<button onClick={() => setAddModalOpen(true)}>Add Transaction</button>
+      //<button onClick={() => setEditModalOpen(true)}>Edit Transaction</button>
 
-      {isAddModalOpen && <AddTransactionModal onClose={() => setAddModalOpen(false)} />}
-      {isEditModalOpen && (
-  <EditTransactionModal
-    transaction={{
-      type: "Income", 
-      amount: "",
-      date: new Date().toISOString().split("T")[0],
-      comment: "",
-    }}
-    onClose={() => setEditModalOpen(false)}
-    onSave={(updatedTransaction) => {
-      console.log("Updated transaction:", updatedTransaction);
-      setEditModalOpen(false);
-    }}
-  />
-)}
+      //{isAddModalOpen && <AddTransactionModal onClose={() => setAddModalOpen(false)} />}
+      //{isEditModalOpen && (
+  //<EditTransactionModal
+    //transaction={{
+     // type: "Income", 
+     // amount: "",
+     // date: new Date().toISOString().split("T")[0],
+     // comment: "",
+   // }}
+    //onClose={() => setEditModalOpen(false)}
+   // onSave={(updatedTransaction) => {
+     // console.log("Updated transaction:", updatedTransaction);
+     // setEditModalOpen(false);
+    //}}
+  // />
+//)}
 
-    </div>
-  );
-};
+    //</div>
+ // );
+//};
 
-export default App;
+//export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,23 +1,23 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; 
-import rootReducer from './Modals/slice'; 
+//import { configureStore } from '@reduxjs/toolkit';
+//import { persistStore, persistReducer } from 'redux-persist';
+//import storage from 'redux-persist/lib/storage'; 
+//import rootReducer from './Modals/slice'; 
 
-const persistConfig = {
-    key: 'root',
-    storage,
-};
+//const persistConfig = {
+  //  key: 'root',
+   // storage,
+//};
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+//const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware({
-            serializableCheck: {
-                ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
-            },
-        }),
-});
+//export const store = configureStore({
+    //reducer: persistedReducer,
+   // middleware: (getDefaultMiddleware) =>
+       // getDefaultMiddleware({
+            //serializableCheck: {
+               // ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+            //},
+        //}),
+//});
 
-export const persistor = persistStore(store);
+//export const persistor = persistStore(store);
